a1ffb952433c11a7a1169efcbe05b85a7d8377e6
Chad Crabtree <chad.rc.crabtree@gmail.com>
2017-11-07 16:26:17 -0500

    initial

---
+++ src/main/java/com/theironyard/springtymeleafproject/SpringtymeleafprojectApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.springtymeleafproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtymeleafprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtymeleafprojectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/theironyard/springtymeleafproject/SpringtymeleafprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.theironyard.springtymeleafproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringtymeleafprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
